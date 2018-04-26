247e1172862f663caedc25f528227e45f4c40657
Fakhruddin Abdi <abdi.fakhruddin@gmail.com>
2018-04-26 11:36:43 +0430

    feat/refactor(Content, Footer):  creaate login form in content component and improve footer style

--- src/Content/Content.js
+++ src/Content/Content.js
@@ -3,6 +3,8 @@
 //------------------------------------------------------------------
 
 import React, { Component } from 'react';
+import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+
 
 // locals
 import s from './Content.style';
@@ -27,8 +29,18 @@ export default class Header extends Component {
     //------------------------------------------    
     render () {
         return (
-            <div>
-                Content
+            <div style={{width: '100%', display: 'flex', justifyContent: 'center', paddingTop: 50}}>
+                <Form style={{width: '500px', backgroundColor: '#eaeaea', padding: '20px', borderRadius: '10px'}}>
+                    <FormGroup>
+                    <Label for="exampleEmail">Email</Label>
+                    <Input type="email" name="email" id="exampleEmail" placeholder="with a placeholder" />
+                    </FormGroup>
+                    <FormGroup>
+                    <Label for="examplePassword">Password</Label>
+                    <Input type="password" name="password" id="examplePassword" placeholder="password placeholder" />
+                    </FormGroup>   
+                    <Button>Submit</Button>
+                 </Form>
             </div>
         )
     }
